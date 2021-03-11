77c85e3e582adac123ebef65c080b267ba27b276
Mina <mina.park127@gmail.com>
2021-03-11 15:43:28 +1100

    get reviewer detail

---
+++ src/routes/ReviewerDetail.tsx
@@ -0,0 +1,52 @@
+import { gql, useQuery } from "@apollo/client";
+import { useParams } from "react-router";
+import { Link } from "react-router-dom";
+import { Review } from "../apiSchema";
+
+interface IParams {
+  reviewer: string;
+}
+
+interface IReviewer {
+  reviewer: Review[];
+}
+
+interface ISearchVars {
+  reviewer: string;
+}
+const GET_REVIEWER = gql`
+  query reviewer($reviewer: String) {
+    reviewer(reviewer: $reviewer) {
+      display_title
+      critics_pick
+    }
+  }
+`;
+
+const ReviewerDetail = () => {
+  const { reviewer } = useParams<IParams>();
+
+  const { loading, error, data } = useQuery<IReviewer, ISearchVars>(
+    GET_REVIEWER,
+    {
+      variables: { reviewer },
+    }
+  );
+
+  return (
+    <>
+      <h2>reviewer detail for {reviewer}</h2>
+      {!loading &&
+        data?.reviewer.map((review, index) => (
+          <section key={index}>
+            <Link to={`/review/${review.display_title}`}>
+              <div>{review.display_title}</div>
+            </Link>
+            <div>{review.critics_pick ? "critic's pick" : "x"}</div>
+          </section>
+        ))}
+    </>
+  );
+};
+
+export default ReviewerDetail;
